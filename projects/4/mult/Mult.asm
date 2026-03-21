a79350e43a508beeefa4a302b3d5cee5c40aa034
Ildar Shamiev <shamief@yandex.ru>
2026-03-21 17:35:48 +0300

    Mult

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -6,5 +6,24 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
-
-//// Replace this comment with your code.
+    @n
+    M=0
+    @R2
+    M=0
+(LOOP)
+    @R1
+    D=M
+    @n
+    D=D-M
+    M=M+1
+    @END
+    D;JEQ
+    @R0
+    D=M
+    @R2
+    M=M+D
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
