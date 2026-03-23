1c12ad0e7c9f13b288ab2d56836bf8d6bbb62c42
Ildar Shamiev <shamief@yandex.ru>
2026-03-23 20:13:46 +0300

    Fill.asm

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -7,5 +7,57 @@
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
-
-//// Replace this comment with your code.
+  @i
+  M=0
+  @8192
+  D=A
+  @n
+  M=D
+(LOOP)
+  @KBD
+  D=M
+  @BLACK
+  D;JNE
+  @SCREEN
+  D=M
+  @WHITE
+  D;JNE
+  @LOOP
+  0;JMP
+(BLACK)
+  @SCREEN
+  D=A
+  @i
+  A=D+M
+  M=-1
+  @i
+  M=M+1
+  D=M
+  @n
+  D=D-M
+  @BLACK
+  D;JLT
+  @i
+  M=0
+  @LOOP
+  0;JMP
+(WHITE)
+  @SCREEN
+  D=A
+  @i
+  A=D+M
+  M=0
+  @i
+  M=M+1
+  D=M
+  @n
+  D=D-M
+  @WHITE
+  D;JLT
+  @i
+  M=0
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
