42c0b1a32d85041cb47129e652faeb3173f2f599
Ildar Shamiev <shamief@yandex.ru>
2026-04-26 20:08:39 +0300

    add Serializer trait, tag_stack

--- projects/10/jackanalyzer/src/serialize.rs
+++ projects/10/jackanalyzer/src/serialize.rs
@@ -1,49 +1,60 @@
-use std::io::{Write, BufRead};
+use std::io::Write;
 use anyhow::Result;
 use quick_xml::Writer;
 use quick_xml::events::{BytesText, BytesEnd, BytesStart, Event};
-use crate::tokenize::JackTokenizer;
-use crate::token::Token;
 
-pub trait TokenSerializer {
-    fn serialize_all<R: BufRead>(&mut self, tokenizer: JackTokenizer<R>) -> Result<()>;
+pub trait Serializer {
+    fn write_name(&mut self, name: &str) -> Result<()>;
+    fn end_name(&mut self) -> Result<()>;
+    fn write_data(&mut self, data: &str) -> Result<()>;
+    fn write_node(&mut self, name: &str, value: &str) -> Result<()>;
+    fn finish(&mut self) -> Result<()>;
 }
 
 pub struct XmlSerializer<W: Write> {
     writer: Writer<W>,
+    tag_stack: Vec<String>,
 }
 
 impl<W: Write> XmlSerializer<W> {
     pub fn new(inner: W) -> Self {
         Self {
-            writer: Writer::new_with_indent(inner, b' ', 4),
+            writer: Writer::new_with_indent(inner, b' ', 2),
+            tag_stack: Vec::new(),
         }
-    }
-
-    fn start_tag(&mut self, name: &str) -> Result<()> {
-        self.writer.write_event(Event::Start(BytesStart::new(name)))?;
-        Ok(())
-    }
-
-    fn end_tag(&mut self, name: &str) -> Result<()> {
-        self.writer.write_event(Event::End(BytesEnd::new(name)))?;
-        Ok(())
     }
 }
 
-impl<W: Write> TokenSerializer for XmlSerializer<W> {
+impl<W: Write> Serializer for XmlSerializer<W> {
+    fn write_name(&mut self, name: &str) -> Result<()> {
+        self.writer.write_event(Event::Start(BytesStart::new(name)))?;
+        self.tag_stack.push(name.to_string());
+        Ok(())
+    }
 
-    fn serialize_all<R: BufRead>( &mut self, mut tokenizer: JackTokenizer<R>) -> Result<()> {
-        self.start_tag("tokens")?;
-        while tokenizer.advance()? {
-            if let Some(token) = Token::from_tokenizer(&mut tokenizer) {
-                let (tag, value) = token.unpack();
-                self.writer.write_event(Event::Start(BytesStart::new(tag)))?;
-                self.writer.write_event(Event::Text(BytesText::new(value)))?;
-                self.writer.write_event(Event::End(BytesEnd::new(tag)))?;
-            }
+    fn end_name(&mut self) -> Result<()> {
+        if let Some(name) = self.tag_stack.pop() {
+            self.writer.write_event(Event::End(BytesEnd::new(name)))?;
         }
-        self.end_tag("tokens")?;
+        Ok(())
+    }
+
+    fn write_data(&mut self, data: &str) -> Result<()> {
+        self.writer.write_event(Event::Text(BytesText::new(data)))?;
+        Ok(())
+    }
+
+    fn write_node(&mut self, name: &str, value: &str) -> Result<()> {
+        self.write_name(name)?;
+        self.write_data(value)?;
+        self.end_name()?;
+        Ok(())
+    }
+
+    fn finish(&mut self) -> Result<()> {
+        while !self.tag_stack.is_empty() {
+            self.end_name()?;
+        }
         Ok(())
     }
 }
