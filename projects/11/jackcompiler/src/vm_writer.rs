347e40565a5983f2779daab0ffcc79cd4573ce70
Ildar Shamiev <shamief@yandex.ru>
2026-05-21 20:59:25 +0300

    add vm writer mod

---
+++ projects/11/jackcompiler/src/vm_writer.rs
@@ -0,0 +1,103 @@
+use std::io::{self, Write, BufWriter};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Segment {
+    Constant,
+    Argument,
+    Local,
+    Static,
+    This,
+    That,
+    Pointer,
+    Temp,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Command {
+    Add,
+    Sub,
+    Neg,
+    Eq,
+    Gt,
+    Lt,
+    And,
+    Or,
+    Not,
+}
+
+pub struct VMWriter<W: Write> {
+    writer: BufWriter<W>,
+}
+
+impl<W: Write> VMWriter<W> {
+    pub fn new(output: W) -> Self {
+        Self {
+            writer: BufWriter::new(output),
+        }
+    }
+
+    fn segment_to_str(segment: Segment) -> &'static str {
+        match segment {
+            Segment::Constant => "constant",
+            Segment::Argument => "argument",
+            Segment::Local => "local",
+            Segment::Static => "static",
+            Segment::This => "this",
+            Segment::That => "that",
+            Segment::Pointer => "pointer",
+            Segment::Temp => "temp",
+        }
+    }
+
+    pub fn write_push(&mut self, segment: Segment, index: usize) -> io::Result<()> {
+        writeln!(self.writer, "push {} {}", Self::segment_to_str(segment), index)
+    }
+
+    pub fn write_pop(&mut self, segment: Segment, index: usize) -> io::Result<()> {
+        writeln!(self.writer, "pop {} {}", Self::segment_to_str(segment), index)
+    }
+
+    pub fn write_arithmetic(&mut self, command: Command) -> io::Result<()> {
+        let cmd_str = match command {
+            Command::Add => "add",
+            Command::Sub => "sub",
+            Command::Neg => "neg",
+            Command::Eq => "eq",
+            Command::Gt => "gt",
+            Command::Lt => "lt",
+            Command::And => "and",
+            Command::Or => "or",
+            Command::Not => "not",
+        };
+        writeln!(self.writer, "{}", cmd_str)
+    }
+
+    pub fn write_label(&mut self, label: &str) -> io::Result<()> {
+        writeln!(self.writer, "label {}", label)
+    }
+
+    pub fn write_goto(&mut self, label: &str) -> io::Result<()> {
+        writeln!(self.writer, "goto {}", label)
+    }
+
+    pub fn write_if(&mut self, label: &str) -> io::Result<()> {
+        writeln!(self.writer, "if-goto {}", label)
+    }
+
+    pub fn write_call(&mut self, label: &str, n_args: usize) -> io::Result<()> {
+        writeln!(self.writer, "call {} {}", label, n_args)
+    }
+
+    pub fn write_function(&mut self, label: &str, n_locals: usize) -> io::Result<()> {
+        writeln!(self.writer, "function {} {}", label, n_locals)
+    }
+
+    pub fn write_return(&mut self) -> io::Result<()> {
+        writeln!(self.writer, "return")
+    }
+
+    pub fn close(&mut self) -> io::Result<()> {
+        self.writer.flush()
+    }
+}
+
